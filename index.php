2ea3db1f29458a1b5426d8f4214c146589dda08f
Rib3llu <eric@zeendoc.com>
2015-12-17 13:12:48 +0100

    formulaire tache chauffeur

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ include "fonctions_annexe.php";
 include "header.php";
 
 //date du jour
-$today = date("Y-m-d");
+$today = date("d-m-Y");
 ?>
 
     <div class="container">
       <div class="starter-template">
-        <h1>Bienvenue sur Transport Altagna</h1>
-        <p class="lead">Servez-vous du menu en haut pour naviguer.<br> Nous sommes le <?php echo $today; ?>.</p>
+        <h1>Bienvenue sur Transport ALTAGNA</h1>
+        <br><p class="lead">Servez-vous du menu en haut pour naviguer.<br><br> Nous sommes le <?php echo $today; ?>.</p>
       </div>
     </div><!-- /.container -->
 
